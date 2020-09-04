085ed4d64520c7fd8585e93dc300ce662e8252e2
4bh <4@4bh.co>
2020-09-04 23:54:08 +0700

    if and while

--- test1.js
+++ test1.js
@@ -7,8 +7,22 @@ actors: {},
 genres: [], privat: false
 }
 
-const a = prompt("One of the latest films?");
-const b = prompt("How would you rate it?")
 
-pmDB.movies[a]=b;
+if (pmDB<10){console.log("Not so impressive!")}
+else if ((30>=pmDB>10)){console.log("Well, that's something...")}
+else{console.log("Error!")}
+
+for (let i = 0, i <2, i++){const a = prompt("One of the latest films?")
+    b = prompt("How would you rate it?")
+
+        if (a != null && b != null && a != '' && a.length < 50) {
+            pmDB.movies[a] = b;
+        } else {
+            i--;
+        }
+
+    }
+
+
+
 
