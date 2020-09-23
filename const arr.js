7a5d73a714b2fcb9154ca638a3018fa702a92cbf
4bh <4@4bh.co>
2020-09-23 13:47:23 +0700

    new line

---
+++ const arr.js
@@ -0,0 +1,11 @@
+const arr = [1, 2, 3, 4];
+
+// for (let i=0; i<arr.length; i++){
+    // console.log(arr[i]);}
+
+//arr.forEach(function(item,i,arr) {
+//console.log(`${i}: ${item} in ${arr}`);})
+
+const str = prompt("","")
+const products = str.split (", ");
+console.log(products.join("; "));
--- test1.js
+++ test1.js
@@ -1,28 +1,33 @@
-let numberOfFilms = +prompt("How much films did you watch?")
-
-const pmDB = {
-count: numberOfFilms,
-movies: {},
-actors: {},
-genres: [], privat: false
-}
+newFunction()
 
 
-if (pmDB<10){console.log("Not so impressive!")}
-else if ((30>=pmDB>10)){console.log("Well, that's something...")}
-else{console.log("Error!")}
 
-for (let i = 0, i <2, i++){const a = prompt("One of the latest films?")
-    b = prompt("How would you rate it?")
 
-        if (a != null && b != null && a != '' && a.length < 50) {
-            pmDB.movies[a] = b;
-        } else {
-            i--;
-        }
+function newFunction() {
+    let numberOfFilms = +prompt("How much films did you watch?")
 
+    const pmDB = {
+        count: numberOfFilms,
+        movies: {},
+        actors: {},
+        genres: [], privat: false
     }
 
 
+    if (pmDB < 10) { console.log("Not so impressive!")} 
+    else if ((30 >= pmDB > 10)) { console.log("Well, that's something...")} 
+    else { console.log("Error!")} 
 
+    for (let i = 0, i; < 2, i++;) {
+        const a = prompt("One of the latest films?")
+        b = prompt("How would you rate it?")
+
+        if (a != null && b != null && a != '' && a.length < 50) {
+            pmDB.movies[a] = b
+        } else {
+            i--
+        }
+
+    }
+}
 
