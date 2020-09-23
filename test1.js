7e87f409a3cb42fedb43c160f6d19c81ff0d5626
4bh <4@4bh.co>
2020-09-23 22:45:57 +0700

    methods

--- test1.js
+++ test1.js
@@ -1,71 +1,61 @@
 
-
-let numberOfFilms;
-
-function start 
-start ();
-
-
 const pmDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [], 
-    privat: false
-    start() => {numberOfFilms = +prompt("How much films did you watch?")
-    while (numberOfFilms === '' || numberOfFilms == null || isNaN(numberOfFilms) )
-    numberOfFilms = +prompt("How much films did you watch?");
-    }
-    
-}
+    privat: false,
 
-function SavePersonalFilms (){
-  for (let i = 0; i < 2; i++) {
-    const a = prompt("One of the latest films?"),
-    b = prompt("How would you rate it?");
+    start: function() { pmDB.count = +prompt("How much films did you watch?")
+    while ( pmDB.count === '' ||  pmDB.count == null || isNaN(pmDB.count) )
+    numberOfFilms = +prompt("How much films did you watch?")},
+     
+    SavePersonalFilms: function (){
+      for (let i = 0; i < 2; i++) {
+        const a = prompt("One of the latest films?"),
+        b = prompt("How would you rate it?");
+      }
+        if(a != null && b != null && a !== '' && a.length < 50){
+        pmDB.movies[a] = b;
+        console.log('Done');
+        } else {
+          console.log('Erroe');
+        i--
+        }
+    },
 
-    if(a != null && b != null && a !== '' && a.length < 50){
-    pmDB.movies[a] = b;
-    console.log('Done');
-    } else {
-      console.log('Erroe');
-    i--
-    }
-}
+    ShowMyDb: function(hidden) {
+      if(!hidden){
+        console.log(pmDB);
+      }
+      ShowMyDb (pmDB.privat);
+      },
 
-SavePersonalFilms();
-
-function DetectPersonalLevel() {
-
-    if(pmDB < 10)
-{console.log("Not so impressive!")}
-else if (30 >= pmDB && pmDB > 10)
-{console.log("Well, that's something...")}
-else
-{console.log("Error!")}
-
-}
-
-DetectPersonalLevel();
-
-function ShowMyDb (hidden) {
-if(!hidden){
-  console.log(pmDB);
-}
-ShowMyDb (pmDB.privat);
-}
-
-ShowMyDb ();
-
-function WriteYourGenres (){
-  for (let i = 1; i => 3; i++){
-    pmDB.genres[i-1] = prompt(`Your fav genre ${i}`);
+    DetectPersonalLevel: function () {
+      if(pmDB < 10) {
+        console.log("Not so impressive!")}
+       else if (30 >= pmDB && pmDB > 10)
+  {console.log("Well, that's something...")}
+  else
+  {console.log("Error!")}
+  
   }
 }
 
-WriteYourGenres ();
 
-}
+    
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
